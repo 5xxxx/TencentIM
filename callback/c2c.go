807527f3c32e095a31ec818a92298780155cac70
lintao <mrqter@gmail.com>
2020-06-01 17:57:33 +0800

    增加回调处理程序

---
+++ callback/c2c.go
@@ -0,0 +1,11 @@
+/*
+ *
+ * c2c.go
+ * callback
+ *
+ * Created by lintao on 2020/6/1 5:16 下午
+ * Copyright © 2020-2020 LINTAO. All rights reserved.
+ *
+ */
+
+package callback
---
+++ callback/group.go
@@ -0,0 +1,11 @@
+/*
+ *
+ * group.go
+ * callback
+ *
+ * Created by lintao on 2020/6/1 5:17 下午
+ * Copyright © 2020-2020 LINTAO. All rights reserved.
+ *
+ */
+
+package callback
